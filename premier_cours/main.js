be996266fd7cd4f5dcb3e566c449ca0e7648c85b
KevinBRIQUET <kevinbriquet@gmx.fr>
2024-02-09 16:09:40 +0100

    First commit

---
+++ premier_cours/main.js
@@ -0,0 +1,15 @@
+// Commentaire
+
+let myButton = document.querySelector("#mon_boutton")
+let mon_age = 34
+
+myButton.addEventListener('click',message)
+console.log(mon_age)
+console.log(typeof(mon_age))
+let prenom = "Kevin"
+console.log(typeof(prenom))
+
+function message()
+{
+    alert("c'est la pause !")
+}
